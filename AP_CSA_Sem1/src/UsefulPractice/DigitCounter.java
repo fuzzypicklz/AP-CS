337f7895ee54ae1d7714b454428b503d25d51996
Fuzzy Pcklz <gronkyd@gmail.com>
2024-09-18 11:40:55 -0700

    9/18/2024 2

---
+++ AP_CSA_Sem1/src/UsefulPractice/DigitCounter.java
@@ -0,0 +1,29 @@
+package UsefulPractice;
+import java.util.Scanner;
+
+public class DigitCounter {
+    public static void main(String[] args){
+        Scanner s  = new Scanner(System.in);
+        while(true){
+            System.out.print("input a number\n> ");
+            String number = s.nextLine();
+            int zeros = 0, evens = 0, odds = 0, sum = 0;
+            for(int i = 0; i<number.length(); i++){
+                System.out.println((int)number.charAt(i) - '0');
+                if((int)number.charAt(i) - '0' == 0){
+                    zeros++;
+                }
+                else if(((int)number.charAt(i) - '0') %2 == 0){
+                    evens++;
+                }
+                else {
+                    odds++;
+                }
+                sum+=(int)number.charAt(i) - '0';
+            }
+            System.out.println("zero digits: "+zeros+"\neven digits: "+evens+"\nodd digits: "+odds+"\nsum: "+sum);
+        }
+        
+    }
+    
+}
